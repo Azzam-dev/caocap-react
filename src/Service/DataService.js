c8dc93dfc531b6b6966e88132ed1e107bda7722a
unknown <faisal.yalthuwaini@gmai.com>
2021-02-20 22:54:02 +0300

    DataService set up .

---
+++ src/Service/DataService.js
@@ -0,0 +1,119 @@
+
+
+
+
+
+class DataService {
+
+
+     updateUserData(uid, userData, handler) {
+        // REF_USERS.child(uid).updateChildValues(userData) 
+    }
+    
+
+     createNewContact(ids, type, handler) {
+
+    }
+    
+    
+     createChat (chatData, handler) {
+        
+    }
+
+     createCaocap (caocapDate, handler) {
+        
+    }
+
+     removeCaocap (key) {
+        
+    }
+    
+     launchCaocap (caocapKey, code) {
+        
+    }
+
+     addAndReomveFromOrbit (caocapKey, remove) {
+        
+    }
+
+     sendRoomMessage (key, messageData, handler) {
+        
+    }
+
+     sendChatMessage (key, messageData, handler) {
+        
+    }
+
+     getCurrentUserChats (handler) {
+        
+    }
+
+     getCurrentUserChatsQuery (query, handler) {
+        
+    }
+
+
+     getCurrentUserCaocaps (handler) {
+        
+    }
+
+    
+
+     getCurrentUserCaocapsQuery (query, handler) {
+        
+    }
+
+
+     getAllCaocaps (handler) {
+        
+    }
+
+
+     getAllPublishedCaocaps (handler) {
+        
+    }
+
+
+     getCaocapsQuery (query, handler) {
+        
+    }
+
+
+     getCaocapRoomMessages (key, handler) {
+        
+    }
+
+
+     getChatMessages (key, handler) {
+        
+    }
+
+
+     getUsernameFromUID (uid, handler) {
+        
+    }
+
+
+     getAllUsernames (handler) {
+        
+    }
+
+
+     getUsernameQuery (query, handler) {
+        
+    }
+
+
+     getUIDs (username, handler) {
+        
+    }
+
+
+     getUserData (handler) {
+        
+    }
+
+
+    
+}
+
